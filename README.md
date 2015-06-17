8d2822e5f24e07a942a3ade1441c46c9e38d27c0
Tim Gasser <timgasser@hotmail.com>
2015-06-17 12:09:14 -0500

    Updated README and completed all the non-optional homework

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 ### EDX Course MITx - 15.071x (Summer 2015)
 
+#### Week 2
+This week focused on Linear Regression, and had a number of assignments. I didn't complete the optional assigments this week due to time. The course is taking around 6 hours a week according to my Toggl summaries.
+
+The hardest thing about this week's assignment was keeping track of how to calculate SSE, SST, RMSE, and R^2 (see below). The lm() and predict() functions were straightforward to use, and like all R functions so far give a nice summary of the model's performance. 
+
+* SSE (Sum of Squared Errors) : Take the sum of the squared differences between the actual and predicted values.
+* SST (Total Sum of Squares) : Disregard all the coefficients of the model apart from the intercept value, and calculate the SSE using this flat line.
+* R^2 = 1 - (SSE / SST) : This quantifies how well the coefficients of the independent variables approximate the real data.
+
+
 #### Week 1
 
 This week's lectures and assignment were an introduction to R and its basic syntax. The assigmnents revolved around loading datasets in CSV format, and finding correlations in the data using the 'table' and 'tapply' commands. Plots were also introduced, with scatter plots, boxplots, and histograms.
--- week2/assignment_flu.R
+++ week2/assignment_flu.R
@@ -11,4 +11,76 @@ FluTrain = read.csv("FluTrain.csv")
 str(FluTrain)
 summary(FluTrain)
 
-table(FluTrain)
+
+
+# 1.2
+hist(FluTrain$ILI)
+
+#1.3
+plot(FluTrain$Queries, log(FluTrain$ILI))
+     
+# 2.2 Dependent var = log(ILI), independent var Queries. Queries coef should be +ve
+FluTrend1 = lm(log(FluTrain$ILI) ~ FluTrain$Queries)
+summary(FluTrend1)
+
+correlation = cor(log(FluTrain$ILI) , FluTrain$Queries) # returns 0.8420333
+
+# 2.3 What is the relationship between correlation and R2?
+correlation^2
+log(1/correlation)
+exp(-0.5*correlation)
+
+# 3.1 
+FluTest = read.csv("FluTest.csv")
+summary(FluTrain)
+summary(FluTest)
+PredTest1 = exp(predict(FluTrend1, newdata=FluTest))
+which(FluTest$Week == "2012-03-11 - 2012-03-17")
+PredTest1[11]
+
+# 3.2
+observed = 2.2934216
+estimated = 2.187383
+
+(observed - estimated) / observed
+
+# 4.1
+ILILag2 = lag(zoo(FluTrain$ILI), -2, na.pad=TRUE)
+FluTrain$ILILag2 = coredata(ILILag2)
+
+ILILag2[1:5]
+FluTrain$ILI[1:5]
+
+# 4.2
+plot(log(ILILag2), log(FluTrain$ILI))
+
+# 4.3
+FluTrend2 = lm(log(FluTrain$ILI) ~ FluTrain$Queries + log(ILILag2))
+summary(FluTrend1)
+summary(FluTrend2)
+
+# Comparing the two models : 
+# R2 1 = 0.709, 2 = 0.9063
+# Significance of coefficients : All are *** (Pr>|t| < 2e-16)
+
+
+# 5.1
+ILILag2 = lag(zoo(FluTest$ILI), -2, na.pad=TRUE)
+FluTest$ILILag2 = coredata(ILILag2)
+
+# 5.3 Need to copy last two training examples into first two test examples
+# These are (from inspection) 1.5181061 1.6639544
+fluTrainRows = nrow(FluTrain)
+FluTest$ILILag2[1] = FluTrain$ILILag2[fluTrainRows-1]
+FluTest$ILILag2[2] = FluTrain$ILILag2[fluTrainRows]
+
+# 5.4
+predictedData = exp(predict(FluTrend2, newdata=FluTest))
+
+# Now predict the out-of-sample R-squared (how well does the model generalise?)
+SSE = sum((predictedData - FluTest$ILI)^2)
+SST = sum((mean(FluTest$ILI) - FluTest$ILI)^2)
+R2 = 1 - (SSE/SST)
+RMSE = sqrt(SSE/nrow(FluTest))
+
+
